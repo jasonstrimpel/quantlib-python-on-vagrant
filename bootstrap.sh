e23d84ac89ef207b14fd11a770773c32591a9e8c
Jason <strimp101@gmail.com>
2017-01-14 14:07:46 +0800

    Made downloads quiet

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,11 @@
 sudo apt-get update -y
 
 # install dependencies
-sudo apt-get install -y libboost-all-dev make automake autoconf libtool python-dev swig git
+sudo apt-get install -y libboost-all-dev g++ make automake autoconf libtool python-dev swig git
 
 # get and install quantlib
 # http://quantlib.org/install/linux.shtml
-git clone https://github.com/lballabio/QuantLib.git
+git clone -q https://github.com/lballabio/QuantLib.git
 cd QuantLib
 ./autogen.sh
 ./configure
@@ -16,13 +16,14 @@ sudo ldconfig
 
 # get and install anaconda python
 # https://www.continuum.io/downloads#_unix
-cd ../
-wget http://repo.continuum.io/archive/Anaconda2-4.1.1-Linux-x86_64.sh -O ~/anaconda.sh
+cd ~
+wget -q http://repo.continuum.io/archive/Anaconda2-4.1.1-Linux-x86_64.sh -O ~/anaconda.sh
 bash ~/anaconda.sh -b -p $HOME/anaconda2
 export PATH="$HOME/anaconda2/bin:$PATH"
 
 # get and install quantlib-swig
 # http://quantlib.org/install/linux-python.shtml
+cd ~
 git clone https://github.com/lballabio/QuantLib-SWIG.git
 cd QuantLib-SWIG
 ./autogen.sh
